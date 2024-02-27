4b73b5827affecb664c7237bc203613ec63ab9a5
Wali Mohammad <kadriwalimohammad@gmail.com>
2024-02-27 18:52:43 +0530

    feat(web): anylytics section

--- app/components/AnalystEstimate.jsx
+++ app/components/AnalystEstimate.jsx
@@ -1,19 +1,20 @@
 export default function AnalystEstimate() {
-  const maxLabelWidth = Math.max('Buy'.length, 'Hold'.length, 'Sell'.length) * 8; // Assuming an average character width of 8px
 
   return (
     <div className='text-lg sm:text-2xl my-2 sm:my-5 flex gap-2 sm:gap-5'>
       <div className='flex items-center justify-center rounded-full text-xl sm:text-3xl h-20 sm:h-32 w-32 sm:w-40 font-bold bg-green-100 text-green-500'>76%</div>
       <div className='w-full flex flex-col justify-center'>
-        {renderEstimate('Buy', 76, 'green', maxLabelWidth)}
-        {renderEstimate('Hold', 8, 'gray', maxLabelWidth)}
-        {renderEstimate('Sell', 16, 'red', maxLabelWidth)}
+      <Estimate label="Buy" percentage={76} color="green" />
+      <Estimate label="Hold" percentage={8} color="gray" />
+      <Estimate label="Sell" percentage={16} color="red" />
       </div>
     </div>
   );
 }
 
-function renderEstimate(label, percentage, color, maxLabelWidth) {
+function Estimate({ label, percentage, color }) {
+  const maxLabelWidth = Math.max('Buy'.length, 'Hold'.length, 'Sell'.length) * 10;
+
   return (
     <div className='flex w-full items-center gap-2 mt-2'>
       <div className='flex-shrink-0 sm:pr-2 text-xs' style={{ width: `${maxLabelWidth}px` }}>{label}</div>
