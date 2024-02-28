ed80318fb216b8cc1d0b94267a5f911590702737
Wali Mohammad <kadriwalimohammad@gmail.com>
2024-02-28 11:51:27 +0530

    feat(UI): Navbar Fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Logo from '../../public/Logo.png'
 import Shape from '../../public/Shape.png'
 
 const NavLink = ({ href, text, className }) => (
-  <Link href={href} className={`hover:bg-gray-300 rounded-md h-10 p-5 flex font-bold items-center ${className}`}>
+  <Link href={href} className={`rounded-md h-10 p-5 flex font-bold items-center ${className}`}>
     {text}
   </Link>
 );
@@ -17,10 +17,10 @@ export default function Navbar() {
         <Image src={Logo} alt='L' width={'auto'} height={'auto'} />
       </div>
       <div className='hidden md:flex'>
-        <NavLink href="#" text="Crypto Tax" />
-        <NavLink href="#" text="Free Tools" />
-        <NavLink href="#" text="Resource Center" />
-        <NavLink href="#" text="Get Started" className="hover:bg-blue-800 bg-blue-700 text-white ml-2" />
+        <NavLink href="#" text="Crypto Tax" className="hover:bg-gray-300" />
+        <NavLink href="#" text="Free Tools" className="hover:bg-gray-300" />
+        <NavLink href="#" text="Resource Center" className="hover:bg-gray-300" />
+        <NavLink href="#" text="Get Started" className="bg-blue-700 text-white ml-2 hover:bg-blue-800" />
       </div>
       <div className='md:hidden'>
        <Image alt='L' src={Shape} />
--- app/section/Sticker.jsx
+++ app/section/Sticker.jsx
@@ -2,12 +2,19 @@ import StickerSVG from "../../public/sticker"
 export default function Sticker() {
   return (
     <div className='flex flex-col bg-blue-700 p-10 text-center rounded-xl gap-5 text-white my-5 md:my-0 items-center'>
-        <div className='text-3xl font-bold'>Get Started With KoinX For FREE</div>
-        <div>With our range of features which you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</div>
+      <div className='text-3xl font-bold'>Get Started With KoinX For FREE</div>
+      <div>With our range of features which you can equip for free, KoinX allows you to be more educated and aware of your tax reports.</div>
+      <div>
+        <StickerSVG />
+      </div>
+      <div className='bg-white p-3 font-bold text-black rounded-md flex items-center gap-1'>
+        <div>Get Started For Free</div>
         <div>
-          <StickerSVG />
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+          </svg>
         </div>
-        <div className='bg-white p-3 font-bold text-black rounded-md'>Get Started For Free</div>
+      </div>
     </div>
   )
 }
